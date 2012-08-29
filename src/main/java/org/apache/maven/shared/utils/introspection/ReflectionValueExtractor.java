a3440f905a301d7ebf8cb24220d5f0cc960ea520
Mark Struberg <struberg@apache.org>
2012-08-29 23:24:31 +0000

    MSHARED-236 add ALv2 (c) ASF ReflectionValueExtractor
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@1378767 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/shared/utils/introspection/ReflectionValueExtractor.java
@@ -0,0 +1,249 @@
+package org.apache.maven.shared.utils.introspection;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.shared.utils.StringUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.WeakHashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * <p>Using simple dotted expressions to extract the values from an Object instance,
+ * For example we might want to extract a value like: <code>project.build.sourceDirectory</code></p>
+ *
+ * <p>The implementation supports indexed, nested and mapped properties similar to the JSP way.</p>
+ *
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl </a>
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id$
+ * @see <a href="http://struts.apache.org/1.x/struts-taglib/indexedprops.html">http://struts.apache.org/1.x/struts-taglib/indexedprops.html</a>
+ */
+public class ReflectionValueExtractor
+{
+    private static final Class[] CLASS_ARGS = new Class[0];
+
+    private static final Object[] OBJECT_ARGS = new Object[0];
+
+    /**
+     * Use a WeakHashMap here, so the keys (Class objects) can be garbage collected.
+     * This approach prevents permgen space overflows due to retention of discarded
+     * classloaders.
+     */
+    private static final Map classMaps = new WeakHashMap();
+
+    /**
+     * Indexed properties pattern, ie <code>(\\w+)\\[(\\d+)\\]</code>
+     */
+    private static final Pattern INDEXED_PROPS = Pattern.compile( "(\\w+)\\[(\\d+)\\]" );
+
+    /**
+     * Indexed properties pattern, ie <code>(\\w+)\\((.+)\\)</code>
+     */
+    private static final Pattern MAPPED_PROPS = Pattern.compile( "(\\w+)\\((.+)\\)" );
+
+    private ReflectionValueExtractor()
+    {
+    }
+
+    /**
+     * <p>The implementation supports indexed, nested and mapped properties.</p>
+     *
+     * <ul>
+     * <li>nested properties should be defined by a dot, i.e. "user.address.street"</li>
+     * <li>indexed properties (java.util.List or array instance) should be contains <code>(\\w+)\\[(\\d+)\\]</code>
+     * pattern, i.e. "user.addresses[1].street"</li>
+     * <li>mapped properties should be contains <code>(\\w+)\\((.+)\\)</code> pattern, i.e. "user.addresses(myAddress).street"</li>
+     * <ul>
+     *
+     * @param expression not null expression
+     * @param root not null object
+     * @return the object defined by the expression
+     * @throws Exception if any
+     */
+    public static Object evaluate( String expression, Object root )
+            throws Exception
+    {
+        return evaluate( expression, root, true );
+    }
+
+    /**
+     * <p>The implementation supports indexed, nested and mapped properties.</p>
+     *
+     * <ul>
+     * <li>nested properties should be defined by a dot, i.e. "user.address.street"</li>
+     * <li>indexed properties (java.util.List or array instance) should be contains <code>(\\w+)\\[(\\d+)\\]</code>
+     * pattern, i.e. "user.addresses[1].street"</li>
+     * <li>mapped properties should be contains <code>(\\w+)\\((.+)\\)</code> pattern, i.e. "user.addresses(myAddress).street"</li>
+     * <ul>
+     *
+     * @param expression not null expression
+     * @param root not null object
+     * @return the object defined by the expression
+     * @throws Exception if any
+     */
+    // TODO: don't throw Exception
+    public static Object evaluate( String expression, Object root, boolean trimRootToken )
+            throws Exception
+    {
+        // if the root token refers to the supplied root object parameter, remove it.
+        if ( trimRootToken )
+        {
+            expression = expression.substring( expression.indexOf( '.' ) + 1 );
+        }
+
+        Object value = root;
+
+        // ----------------------------------------------------------------------
+        // Walk the dots and retrieve the ultimate value desired from the
+        // MavenProject instance.
+        // ----------------------------------------------------------------------
+
+        StringTokenizer parser = new StringTokenizer( expression, "." );
+
+        while ( parser.hasMoreTokens() )
+        {
+            // if we have nothing, stop now
+            if ( value == null )
+            {
+                return null;
+            }
+
+            String token = parser.nextToken();
+
+            ClassMap classMap = getClassMap( value.getClass() );
+
+            Method method;
+            Object[] localParams = OBJECT_ARGS;
+
+            // do we have an indexed property?
+            Matcher matcher = INDEXED_PROPS.matcher( token );
+            if ( matcher.find() )
+            {
+                String methodBase = StringUtils.capitalizeFirstLetter( matcher.group( 1 ) );
+                String methodName = "get" + methodBase;
+                method = classMap.findMethod( methodName, CLASS_ARGS );
+                value = method.invoke( value, OBJECT_ARGS );
+                classMap = getClassMap( value.getClass() );
+
+                if ( classMap.getCachedClass().isArray() )
+                {
+                    value = Arrays.asList( (Object[]) value );
+                    classMap = getClassMap( value.getClass() );
+                }
+
+                if ( value instanceof List )
+                {
+                    // use get method on List interface
+                    localParams = new Object[1];
+                    localParams[0] = Integer.valueOf( matcher.group( 2 ) );
+                    method = classMap.findMethod( "get", localParams );
+                }
+                else
+                {
+                    throw new Exception( "The token '" + token
+                            + "' refers to a java.util.List or an array, but the value seems is an instance of '"
+                            + value.getClass() + "'." );
+                }
+            }
+            else
+            {
+                // do we have a mapped property?
+                matcher = MAPPED_PROPS.matcher( token );
+                if ( matcher.find() )
+                {
+                    String methodBase = StringUtils.capitalizeFirstLetter( matcher.group( 1 ) );
+                    String methodName = "get" + methodBase;
+                    method = classMap.findMethod( methodName, CLASS_ARGS );
+                    value = method.invoke( value, OBJECT_ARGS );
+                    classMap = getClassMap( value.getClass() );
+
+                    if ( value instanceof Map )
+                    {
+                        // use get method on List interface
+                        localParams = new Object[1];
+                        localParams[0] = matcher.group( 2 );
+                        method = classMap.findMethod( "get", localParams );
+                    }
+                    else
+                    {
+                        throw new Exception( "The token '" + token
+                                + "' refers to a java.util.Map, but the value seems is an instance of '"
+                                + value.getClass() + "'." );
+                    }
+                }
+                else
+                {
+                    String methodBase = StringUtils.capitalizeFirstLetter( token );
+                    String methodName = "get" + methodBase;
+                    method = classMap.findMethod( methodName, CLASS_ARGS );
+
+                    if ( method == null )
+                    {
+                        // perhaps this is a boolean property??
+                        methodName = "is" + methodBase;
+
+                        method = classMap.findMethod( methodName, CLASS_ARGS );
+                    }
+                }
+            }
+
+            if ( method == null )
+            {
+                return null;
+            }
+
+            try
+            {
+                value = method.invoke( value, localParams );
+            }
+            catch ( InvocationTargetException e )
+            {
+                // catch array index issues gracefully, otherwise release
+                if ( e.getCause() instanceof IndexOutOfBoundsException )
+                {
+                    return null;
+                }
+
+                throw e;
+            }
+        }
+
+        return value;
+    }
+
+    private static ClassMap getClassMap( Class clazz )
+    {
+        ClassMap classMap = (ClassMap) classMaps.get( clazz );
+
+        if ( classMap == null )
+        {
+            classMap = new ClassMap( clazz );
+
+            classMaps.put( clazz, classMap );
+        }
+
+        return classMap;
+    }
+}
---
+++ src/test/java/org/apache/maven/shared/utils/introspection/ReflectionValueExtractorTest.java
@@ -0,0 +1,298 @@
+package org.apache.maven.shared.utils.introspection;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id$
+ */
+public class ReflectionValueExtractorTest
+    extends TestCase
+{
+    private Project project;
+
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        Dependency dependency1 = new Dependency();
+        dependency1.setArtifactId("dep1");
+        Dependency dependency2 = new Dependency();
+        dependency2.setArtifactId("dep2");
+
+        project = new Project();
+        project.setModelVersion( "4.0.0" );
+        project.setGroupId( "org.apache.maven" );
+        project.setArtifactId( "maven-core" );
+        project.setName( "Maven" );
+        project.setVersion( "2.0-SNAPSHOT" );
+        project.setScm( new Scm() );
+        project.getScm().setConnection( "scm-connection" );
+        project.addDependency( dependency1 );
+        project.addDependency( dependency2 );
+        project.setBuild( new Build() );
+    }
+
+    public void testValueExtraction()
+        throws Exception
+    {
+        // ----------------------------------------------------------------------
+        // Top level values
+        // ----------------------------------------------------------------------
+
+        assertEquals( "4.0.0", ReflectionValueExtractor.evaluate( "project.modelVersion", project ) );
+
+        assertEquals( "org.apache.maven", ReflectionValueExtractor.evaluate( "project.groupId", project ) );
+
+        assertEquals( "maven-core", ReflectionValueExtractor.evaluate( "project.artifactId", project ) );
+
+        assertEquals( "Maven", ReflectionValueExtractor.evaluate( "project.name", project ) );
+
+        assertEquals( "2.0-SNAPSHOT", ReflectionValueExtractor.evaluate( "project.version", project ) );
+
+        // ----------------------------------------------------------------------
+        // SCM
+        // ----------------------------------------------------------------------
+
+        assertEquals( "scm-connection", ReflectionValueExtractor.evaluate( "project.scm.connection", project ) );
+
+        // ----------------------------------------------------------------------
+        // Dependencies
+        // ----------------------------------------------------------------------
+
+        List dependencies = (List) ReflectionValueExtractor.evaluate( "project.dependencies", project );
+
+        Assert.assertNotNull( dependencies );
+
+        Assert.assertEquals( 2, dependencies.size() );
+
+        // ----------------------------------------------------------------------
+        // Dependencies - using index notation
+        // ----------------------------------------------------------------------
+
+        // List
+        Dependency dependency = (Dependency)ReflectionValueExtractor.evaluate( "project.dependencies[0]", project );
+
+        Assert.assertNotNull( dependency );
+
+        Assert.assertTrue( "dep1".equals(dependency.getArtifactId()) );
+
+        String artifactId = (String)ReflectionValueExtractor.evaluate( "project.dependencies[1].artifactId", project );
+
+        Assert.assertTrue( "dep2".equals(artifactId) );
+
+        // Array
+
+        dependency = (Dependency)ReflectionValueExtractor.evaluate( "project.dependenciesAsArray[0]", project );
+
+        Assert.assertNotNull( dependency );
+
+        Assert.assertTrue( "dep1".equals(dependency.getArtifactId()) );
+
+        artifactId = (String)ReflectionValueExtractor.evaluate( "project.dependenciesAsArray[1].artifactId", project );
+
+        Assert.assertTrue( "dep2".equals(artifactId) );
+
+        // Map
+
+        dependency = (Dependency)ReflectionValueExtractor.evaluate( "project.dependenciesAsMap(dep1)", project );
+
+        Assert.assertNotNull( dependency );
+
+        Assert.assertTrue( "dep1".equals(dependency.getArtifactId()) );
+
+        artifactId = (String)ReflectionValueExtractor.evaluate( "project.dependenciesAsMap(dep2).artifactId", project );
+
+        Assert.assertTrue( "dep2".equals(artifactId) );
+
+        // ----------------------------------------------------------------------
+        // Build
+        // ----------------------------------------------------------------------
+
+        Build build = (Build) ReflectionValueExtractor.evaluate( "project.build", project );
+
+        Assert.assertNotNull( build );
+    }
+
+    public void testValueExtractorWithAInvalidExpression()
+        throws Exception
+    {
+        Assert.assertNull( ReflectionValueExtractor.evaluate( "project.foo", project ) );
+        Assert.assertNull( ReflectionValueExtractor.evaluate( "project.dependencies[10]", project ) );
+        Assert.assertNull( ReflectionValueExtractor.evaluate( "project.dependencies[0].foo", project ) );
+    }
+
+    public static class Project
+    {
+        private String modelVersion;
+
+        private String groupId;
+
+        private Scm scm;
+
+        private List dependencies = new ArrayList();
+
+        private Build build;
+
+        private String artifactId;
+
+        private String name;
+
+        private String version;
+
+        public void setModelVersion( String modelVersion )
+        {
+            this.modelVersion = modelVersion;
+        }
+
+        public void setGroupId( String groupId )
+        {
+            this.groupId = groupId;
+        }
+
+        public void setScm( Scm scm )
+        {
+            this.scm = scm;
+        }
+
+        public void addDependency( Dependency dependency )
+        {
+            this.dependencies.add( dependency );
+        }
+
+        public void setBuild( Build build )
+        {
+            this.build = build;
+        }
+
+        public void setArtifactId( String artifactId )
+        {
+            this.artifactId = artifactId;
+        }
+
+        public void setName( String name )
+        {
+            this.name = name;
+        }
+
+        public void setVersion( String version )
+        {
+            this.version = version;
+        }
+
+        public Scm getScm()
+        {
+            return scm;
+        }
+
+        public String getModelVersion()
+        {
+            return modelVersion;
+        }
+
+        public String getGroupId()
+        {
+            return groupId;
+        }
+
+        public List getDependencies()
+        {
+            return dependencies;
+        }
+
+        public Build getBuild()
+        {
+            return build;
+        }
+
+        public String getArtifactId()
+        {
+            return artifactId;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getVersion()
+        {
+            return version;
+        }
+
+        public Dependency[] getDependenciesAsArray()
+        {
+            return (Dependency[]) getDependencies().toArray( new Dependency[0]);
+        }
+
+        public Map getDependenciesAsMap()
+        {
+            Map ret = new HashMap();
+            for ( Iterator it = getDependencies().iterator(); it.hasNext();)
+            {
+                Dependency dep = (Dependency)it.next();
+                ret.put( dep.getArtifactId(), dep );
+            }
+            return ret;
+        }
+    }
+
+    public static class Build
+    {
+
+    }
+
+    public static class Dependency
+    {
+        private String artifactId;
+
+        public String getArtifactId()
+        {
+            return artifactId;
+        }
+
+        public void setArtifactId(String id)
+        {
+            artifactId = id;
+        }
+    }
+
+    public static class Scm
+    {
+        private String connection;
+
+        public void setConnection( String connection )
+        {
+            this.connection = connection;
+        }
+
+        public String getConnection()
+        {
+            return connection;
+        }
+    }
+}
