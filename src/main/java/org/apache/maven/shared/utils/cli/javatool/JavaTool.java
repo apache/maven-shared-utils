7e547dd500ebea5438009935b17be3c59a0c8145
Elliotte Rusty Harold <elharo@ibiblio.org>
2023-03-05 17:21:27 -0500

    remove duplicate licenses

--- src/main/java/org/apache/maven/shared/utils/cli/javatool/JavaTool.java
+++ src/main/java/org/apache/maven/shared/utils/cli/javatool/JavaTool.java
@@ -18,25 +18,6 @@
  */
 package org.apache.maven.shared.utils.cli.javatool;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 /**
  * <p>Describes a java tool, means a executable available in the jdk.</p>
  * <p>The name of the tool ({@link #getJavaToolName()}) reflects the name of the executable that should exists as an
