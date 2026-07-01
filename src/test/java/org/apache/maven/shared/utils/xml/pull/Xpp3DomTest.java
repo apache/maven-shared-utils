e6f70f03ae6f8c066c88597db60bd7e25d05af97
Elliotte Rusty Harold <elharo@ibiblio.org>
2026-07-01 09:02:06 -0400

    spotless:apply formatting

--- src/test/java/org/apache/maven/shared/utils/xml/pull/Xpp3DomTest.java
+++ src/test/java/org/apache/maven/shared/utils/xml/pull/Xpp3DomTest.java
@@ -28,7 +28,6 @@ import static org.apache.maven.shared.utils.xml.Xpp3Dom.mergeXpp3Dom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
