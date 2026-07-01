e2ebb66198e4c0151147e6ab9f62ae07039f34f2
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2026-07-01 19:21:24 +0000

    StringUtils leftPad/rightPad: handle empty delimiter without ArithmeticException (#409)
    * StringUtils leftPad/rightPad: handle empty delimiter without ArithmeticException
    
    * remove @throws ArithmeticException from leftPad/rightPad javadoc

--- src/main/java/org/apache/maven/shared/utils/StringUtils.java
+++ src/main/java/org/apache/maven/shared/utils/StringUtils.java
@@ -1111,11 +1111,13 @@ public class StringUtils {
      * @param size  size to pad to
      * @param delim string to pad with
      * @return right padded String
-     * @throws ArithmeticException  if delim is the empty String
      * @throws NullPointerException if str or delim is <code>null</code>
      */
     @NonNull
     public static String rightPad(@NonNull String str, int size, @NonNull String delim) {
+        if (delim.isEmpty()) {
+            return str;
+        }
         size = (size - str.length()) / delim.length();
         if (size > 0) {
             str += repeat(delim, size);
@@ -1145,11 +1147,13 @@ public class StringUtils {
      * @param size  size to pad to
      * @param delim string to pad with
      * @return left padded String
-     * @throws ArithmeticException  if delim is the empty string
      * @throws NullPointerException if str or delim is null
      */
     @NonNull
     public static String leftPad(@NonNull String str, int size, @NonNull String delim) {
+        if (delim.isEmpty()) {
+            return str;
+        }
         size = (size - str.length()) / delim.length();
         if (size > 0) {
             str = repeat(delim, size) + str;
--- src/test/java/org/apache/maven/shared/utils/StringUtilsTest.java
+++ src/test/java/org/apache/maven/shared/utils/StringUtilsTest.java
@@ -859,6 +859,11 @@ public class StringUtilsTest {
         assertEquals("*****dings", StringUtils.leftPad("dings", 10, "*"));
     }
 
+    @Test
+    public void testLeftPadEmptyDelim() {
+        assertEquals("dings", StringUtils.leftPad("dings", 10, ""));
+    }
+
     @SuppressWarnings("ConstantValue")
     @Test
     public void testLowerCase() {
@@ -1271,6 +1276,11 @@ public class StringUtilsTest {
         assertEquals("dings", StringUtils.rightPad("dings", 3, "+"));
 
         assertEquals("dings+++++", StringUtils.rightPad("dings", 10, "+"));
+    }
+
+    @Test
+    public void testRightPadEmptyDelim() {
+        assertEquals("dings", StringUtils.rightPad("dings", 10, ""));
     }
 
     @Test
